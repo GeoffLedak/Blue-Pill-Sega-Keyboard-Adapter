0b85e5790c80fc65e8920b13fce250e2aaa4bdeb
Geoff Ledak <geoffledak@gmail.com>
2019-03-28 12:05:04 -0600

    receiving 0xFF from Sega now resets LED and typematic status on keyboard

--- blue_pill_keyboard.ino
+++ blue_pill_keyboard.ino
@@ -93,7 +93,7 @@ volatile uint8_t resendTimeout = 0;
 
 void setup()
 {
-    // Serial.begin(9600);
+    Serial.begin(9600);
     
     // Setup AT keyboard communication
     
@@ -531,8 +531,8 @@ void Listen_To_Sega()
     }
     
     
-    // Serial.print("FROM SEGA ");
-    // Serial.println(incomingValue, HEX);
+    Serial.print("FROM SEGA ");
+    Serial.println(incomingValue, HEX);
     
     
     endWait();                              // wait for start to go up
@@ -729,7 +729,7 @@ void ps2interrupt()
                 if( _parity == val )     // Both same parity error
                 {
                     hasParityError = 1;
-                    // Serial.println("P.E ====");
+                    Serial.println("P.E ====");
                 } 
         
                 break;
@@ -848,11 +848,11 @@ void sendEnableScanCommand()
 
 void processByteFromKeyboard()
 {
-    // Serial.println(incoming, HEX);
+    Serial.println(incoming, HEX);
 
     if( incoming == 0xFF || incoming == 0x00 )
     {
-        // Serial.println("SHIET");
+        Serial.println("SHIET");
         // shit is fucked. Reset everything
         requestResendFromKeyboard = 0;
         clearSendBuffer();
@@ -941,9 +941,28 @@ void processByteFromKeyboard()
 
 void processByteFromSega(uint8_t incomingValue)
 {
+    
+    if( incomingValue == 0xFF )
+    {
+        LEDstatusByte = 0;
+        LEDstatusIncoming = 0;
+
+        TypematicStatusByte = 0x0000002B;
+        TypematicStatusIncoming = 0;
+        
+        clearSendBuffer();
+        
+        addByteToSendBuffer(0xED);
+        addByteToSendBuffer(LEDstatusByte);
+        
+        addByteToSendBuffer(0xF3);
+        addByteToSendBuffer(TypematicStatusByte);
+    }
+    
+    
     // Keyboard LEDs
     
-    if( incomingValue == 0xED )
+    else if( incomingValue == 0xED )
     {
         LEDstatusIncoming = 1;
     }
@@ -1003,7 +1022,7 @@ void sendTypematicAndLEDs()
 
 void handleParityErrorFromKeyboard()
 {
-    // Serial.println("P error");
+    Serial.println("P error");
 
     waitingForAck = 0;
     requestResendFromKeyboard = 1;
