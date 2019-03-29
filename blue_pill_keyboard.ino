beaf51708b511c0c550e39d7c5ea3f80d783e450
Geoff Ledak <geoffledak@gmail.com>
2019-03-28 21:24:18 -0600

    Commented out Serial stuff

--- blue_pill_keyboard.ino
+++ blue_pill_keyboard.ino
@@ -93,7 +93,7 @@ volatile uint8_t resendTimeout = 0;
 
 void setup()
 {
-    Serial.begin(9600);
+    // Serial.begin(9600);
     
     // Setup AT keyboard communication
     
@@ -531,8 +531,8 @@ void Listen_To_Sega()
     }
     
     
-    Serial.print("FROM SEGA ");
-    Serial.println(incomingValue, HEX);
+    // Serial.print("FROM SEGA ");
+    // Serial.println(incomingValue, HEX);
     
     
     endWait();                              // wait for start to go up
@@ -729,7 +729,7 @@ void ps2interrupt()
                 if( _parity == val )     // Both same parity error
                 {
                     hasParityError = 1;
-                    Serial.println("P.E ====");
+                    // Serial.println("P.E ====");
                 } 
         
                 break;
@@ -848,11 +848,11 @@ void sendEnableScanCommand()
 
 void processByteFromKeyboard()
 {
-    Serial.println(incoming, HEX);
+    // Serial.println(incoming, HEX);
 
     if( incoming == 0xFF || incoming == 0x00 )
     {
-        Serial.println("SHIET");
+        // Serial.println("SHIET");
         // shit is fucked. Reset everything
         requestResendFromKeyboard = 0;
         clearSendBuffer();
@@ -941,6 +941,7 @@ void processByteFromKeyboard()
 
 void processByteFromSega(uint8_t incomingValue)
 {
+    // Reset LED and Typematic status
     
     if( incomingValue == 0xFF )
     {
@@ -1022,7 +1023,7 @@ void sendTypematicAndLEDs()
 
 void handleParityErrorFromKeyboard()
 {
-    Serial.println("P error");
+    // Serial.println("P error");
 
     waitingForAck = 0;
     requestResendFromKeyboard = 1;
